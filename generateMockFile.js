4f633eaf2192ba7fb59d5d96a67c8e18e1e184a6
Olof Fredriksson <fredriksson@olof.nu>
2021-01-07 19:32:04 +0100

    generate mock.json into dist folder

--- generateMockFile.js
+++ generateMockFile.js
@@ -4,9 +4,9 @@ const path = require("path");
 const glob = require("glob");
 const mockFile = {};
 
-glob.sync("dist/**", { nodir: true }).forEach((filePath) => {
+glob.sync("dist/**/*.js", { nodir: true }).forEach((filePath) => {
     const name = path.basename(filePath, ".js");
     mockFile[name] = `<rootDir>/node_modules/${pkg.name}/${filePath}`;
 });
 
-fs.writeJSONSync("mock.json", mockFile);
+fs.writeJSONSync("dist/mock.json", mockFile);
