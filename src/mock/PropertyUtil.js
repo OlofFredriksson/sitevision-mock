29eee09f28e8ba789e640d4b1fb2e97b08b85f3b
Olof Fredriksson <fredriksson@olof.nu>
2021-04-08 19:44:37 +0200

    Reference directly to internal functions

--- src/mock/PropertyUtil.js
+++ src/mock/PropertyUtil.js
@@ -6,30 +6,14 @@ import { generateMock } from "../utils";
  it is up to consumer to define correct values
 */
 export default generateMock({
-    getNode: function (...args) {
-        return getProperty(...args);
-    },
-    getString: function (...args) {
-        return getProperty(...args);
-    },
-    getBoolean: function (...args) {
-        return getProperty(...args);
-    },
-    getCalendar: function (...args) {
-        return getProperty(...args);
-    },
-    getInt: function (...args) {
-        return getProperty(...args);
-    },
-    getStringEscaped: function (...args) {
-        return getProperty(...args);
-    },
-    getNestedString: function (...args) {
-        return getNestedProperty(...args);
-    },
-    getNestedBinary: function (...args) {
-        return getNestedProperty(...args);
-    },
+    getNode: getProperty,
+    getString: getProperty,
+    getBoolean: getProperty,
+    getCalendar: getProperty,
+    getInt: getProperty,
+    getStringEscaped: getProperty,
+    getNestedString: getNestedProperty,
+    getNestedBinary: getNestedProperty,
 });
 
 function getProperty(node, property, defaultValue = null) {
