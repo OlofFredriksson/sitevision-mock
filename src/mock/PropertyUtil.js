a607a0ac6195d8575a9b9b242abb747fc869d304
Olof Fredriksson <fredriksson@olof.nu>
2021-04-08 16:24:10 +0200

    Mock PropertyUtil functions

--- src/mock/PropertyUtil.js
+++ src/mock/PropertyUtil.js
@@ -1,9 +1,52 @@
 // https://developer.sitevision.se/webdav/files/apidocs/senselogic/sitevision/api/property/PropertyUtil.html
 import { generateMock } from "../utils";
 
+/*
+ Functions wont transform return value to correct type,
+ it is up to consumer to define correct values
+*/
 export default generateMock({
-    // Gets a Node property from a Node.s
-    getNode: function (node, property) {
-        return node[property];
+    getNode: function (...args) {
+        return getProperty(...args);
+    },
+    getString: function (...args) {
+        return getProperty(...args);
+    },
+    getBoolean: function (...args) {
+        return getProperty(...args);
+    },
+    getCalendar: function (...args) {
+        return getProperty(...args);
+    },
+    getInt: function (...args) {
+        return getProperty(...args);
+    },
+    getStringEscaped: function (...args) {
+        return getProperty(...args);
+    },
+    getNestedString: function (...args) {
+        return getNestedProperty(...args);
+    },
+    getNestedBinary: function (...args) {
+        return getNestedProperty(...args);
     },
 });
+
+function getProperty(node, property, defaultValue = null) {
+    if (!node) {
+        return defaultValue;
+    }
+    return node[property] ? node[property] : defaultValue;
+}
+
+function getNestedProperty(
+    node,
+    nodePropertyName,
+    property,
+    defaultValue = null
+) {
+    if (!node || !node[nodePropertyName]) {
+        return defaultValue;
+    }
+    return getProperty(node[nodePropertyName], property, defaultValue);
+}
---
+++ src/mock/PropertyUtil.spec.js
@@ -0,0 +1,97 @@
+import propertyUtil from "./PropertyUtil";
+
+let node;
+
+beforeAll(() => {
+    node = {
+        property: "foo:bar", // Type is not relevant since mock dont transform the return value
+        nestedProperty: {
+            property: "foo:bar", // Type is not relevant since mock dont transform the return value
+        },
+    };
+});
+
+describe("PropertyUtil", () => {
+    // Functions that internal use getProperty();
+    describe("non nested property", () => {
+        const functions = [
+            "getNode",
+            "getString",
+            "getBoolean",
+            "getCalendar",
+            "getInt",
+            "getStringEscaped",
+        ];
+
+        test.each(functions)(
+            "%s should return null when missing ID or parameters is missing",
+            (fn) => {
+                expect(propertyUtil[fn]({})).toEqual(null);
+                expect(propertyUtil[fn](null, null)).toEqual(null);
+                expect(propertyUtil[fn](undefined, undefined)).toEqual(null);
+            }
+        );
+        test.each(functions)(
+            "%s should return custom default value if data not found",
+            (fn) => {
+                expect(propertyUtil[fn](undefined, undefined, "foo")).toEqual(
+                    "foo"
+                );
+            }
+        );
+
+        test.each(functions)(
+            "%s should return null when missing given property",
+            (fn) => {
+                expect(propertyUtil[fn](node, "missingProperty")).toEqual(null);
+            }
+        );
+
+        test.each(functions)(
+            "%s should return actual value when found",
+            (fn) => {
+                expect(propertyUtil[fn](node, "property")).toEqual("foo:bar");
+            }
+        );
+    });
+
+    // Functions that internal use getNestedProperty();
+    describe("getNestedProperty", () => {
+        const functions = ["getNestedString", "getNestedBinary"];
+
+        test.each(functions)(
+            "%s should return null when missing ID or parameters is missing",
+            (fn) => {
+                expect(propertyUtil[fn]({})).toEqual(null);
+                expect(propertyUtil[fn](null, null)).toEqual(null);
+                expect(propertyUtil[fn](undefined, undefined)).toEqual(null);
+            }
+        );
+        test.each(functions)(
+            "%s should return custom default value if data not found",
+            (fn) => {
+                expect(
+                    propertyUtil[fn](undefined, undefined, undefined, "foo")
+                ).toEqual("foo");
+            }
+        );
+
+        test.each(functions)(
+            "%s should return null when missing given property",
+            (fn) => {
+                expect(
+                    propertyUtil[fn](node, "missingProperty", "property")
+                ).toEqual(null);
+            }
+        );
+
+        test.each(functions)(
+            "%s should return actual value when found",
+            (fn) => {
+                expect(
+                    propertyUtil[fn](node, "nestedProperty", "property")
+                ).toEqual("foo:bar");
+            }
+        );
+    });
+});
