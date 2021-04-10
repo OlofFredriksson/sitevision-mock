8649cfb1da953395eb6920ce655bf7a63e818faa
Olof Fredriksson <fredriksson@olof.nu>
2021-04-10 11:32:10 +0200

    properties.get

--- src/mock/Properties.js
+++ src/mock/Properties.js
@@ -1,7 +1,16 @@
+// https://developer.sitevision.se/webdav/files/apidocs/senselogic/sitevision/api/property/Properties.html
 import { generateMock } from "../utils";
 
 export default generateMock({
-    get: function (node) {
-        return node ? node.uuid : undefined;
+    get: function (...args) {
+        if (args.length === 1 && args[0]) {
+            const node = args[0];
+            return node.uuid ? node.uuid : null;
+        }
+        if (args.length >= 2) {
+            // No need to return the specific keys
+            return args[0];
+        }
+        return null;
     },
 });
--- src/mock/Properties.spec.js
+++ src/mock/Properties.spec.js
@@ -2,12 +2,17 @@ import properties from "./Properties";
 
 describe("get", () => {
     test("should return undefined when missing ID", () => {
-        expect(properties.get({})).toEqual(undefined);
-        expect(properties.get(null)).toEqual(undefined);
-        expect(properties.get(undefined)).toEqual(undefined);
+        expect(properties.get({})).toEqual(null);
+        expect(properties.get(null)).toEqual(null);
+        expect(properties.get(undefined)).toEqual(null);
     });
     test("with ID", () => {
         const node = { uuid: "id" };
         expect(properties.get(node)).toEqual("id");
+    });
+
+    test("should return same object when more than one parameter is used", () => {
+        const node = { uuid: "id", URI: "/4.5***.html" };
+        expect(properties.get(node, "URI", "displayName")).toEqual(node);
     });
 });
