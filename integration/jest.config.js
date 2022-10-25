00d842dde5ccc4fe4680b081981b45aeba227389
Olof Fredriksson <fredriksson@olof.nu>
2022-10-25 19:27:29 +0200

    Specify babel-jest for integration tests

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,8 +1,15 @@
+const path = require("path");
 module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ["./**/*.js", "!**/coverage/**", "!**/jest.config.js"],
     /* You need to build before running these tests */
     moduleNameMapper: {
         ...require("../dist/mock"),
+    },
+    transform: {
+        "\\.js$": [
+            "babel-jest",
+            { configFile: path.join(__dirname, "../babel.config.js") },
+        ],
     },
 };
