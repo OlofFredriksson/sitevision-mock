db731264999d6ab33811a5c3ae9bd0ba7032a5dd
Olof Fredriksson <fredriksson@olof.nu>
2022-12-29 12:45:43 +0100

    minimum node v12

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
         [
             "@babel/preset-env",
             {
-                targets: { node: "10" },
+                targets: { node: "14" },
             },
         ],
     ],
